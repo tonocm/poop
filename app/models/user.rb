19fcfa5dff39c79f321962bb0fe5a2b55ce5e6c0
tonocm <tonocm94@hotmail.com>
2014-10-24 04:54:18 -0400

    Messed Around with The Database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
 
-validates_uniqueness_of :username
-validates_presence_of :name
-validates_confirmation_of :password
-validates_length_of :password, minimum: 6, message: "must be at least 6 characters long"
+#validates_uniqueness_of :username
+#validates_presence_of :name
+validates :name, presence: true, length: {maximum: 50}
+validates :email, presence: true, length: {maximum: 255}, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: {case_sensitive: false}
+validates :username, uniqueness: {case_sensitive: false}
+#validates_confirmation_of :password
+#validates_length_of :password, minimum: 6, message: "must be at least 6 characters long"
+has_secure_password
+validates :password, length: {minimum: 6}
 end
--- db/migrate/20141010013949_create_users.rb
+++ db/migrate/20141010013949_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :username
       t.string :name
       t.string :email
-      t.string :password
+      t.string :password_digest
 
       t.timestamps
     end
