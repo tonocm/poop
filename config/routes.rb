b875d0c527e95a6dc690359104457cd021920b9b
tonocm <tonocm94@hotmail.com>
2014-10-28 03:41:22 -0400

    Added Welcome

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
+  get    'welcome' => 'sessions#welcome'
   #end
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
