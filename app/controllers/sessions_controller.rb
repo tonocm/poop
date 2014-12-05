0c687e0dfc8419e79691b89bcb174ef1bb67e8f4
tonocm <tonocm94@hotmail.com>
2014-12-05 12:45:46 -0500

    Adding Firebase

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,17 @@ class SessionsController < ApplicationController
 
   def new
     if logged_in?
+
+      base_uri = 'https://hacker-news.firebaseio.com'
+
+      firebase = Firebase::Client.new(base_uri)
+
+      response = firebase.push("todos", { :name => 'Pick the milk', :priority => 1 })
+      response.success? # => true
+      response.code # => 200
+      response.body # => { 'name' => "-INOQPH-aV_psbk3ZXEX" }
+      response.raw_body # => '{"name":"-INOQPH-aV_psbk3ZXEX"}''
+
       redirect_to welcome_path
     end
   end
